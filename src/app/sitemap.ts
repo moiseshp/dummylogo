9e824e78ed59c88ba8404aa5d6a028c2d251922a
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2024-12-30 16:03:57 -0500

    fix: add base url hardcode

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,15 @@
-import { BASE_URL } from '@/lib/config';
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: BASE_URL,
+      url: 'https://dummylogo.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `${BASE_URL}/blog`,
+      url: 'https://dummylogo.vercel.app/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
